7a4c8041a76b547c726e0c965993024eb4e5a2a6
gabe <GabeMaker@users.noreply.github.com>
2015-01-02 13:59:32 +0000

    more spiking and notes about how this project might work

--- lib/takeaway_spike.rb
+++ lib/takeaway_spike.rb
@@ -1,5 +1,6 @@
-# keep it simple, 2 choices for main, side and drink
+# FUNCTIONALITY 1: list of dishes with prices
 
+# keep it simple, 2 choices for main, side and drink
 DISHLIST = {pizza: 12, pasta: 10, salad: 7.50, garlic_bread: 5, cola: 2.5, mineral_water: 2}
 
 def print_dishlist
@@ -8,3 +9,29 @@ def print_dishlist
 end
 
 print_dishlist
+
+# FUNCTIONALITY 2:
+
+  # a) "Placing the order by giving the list of dishes, their quantities and a number that should be the exact total"
+
+      # my initial and very literal interpretation of this functionality:
+
+        # order( (pizza, 2), garlic_bread, cola, 31.5)
+
+      # maybe more like:
+
+        # add_dish (pizza, 2)
+        # add_dish (garlic_bread)     <- 1 implied if no 2nd argument given
+        # add_dish (cola)
+        
+      # involving adding dishes to an array (my_order). Could also add dishes by number. Then
+
+        # order (my_order, 31.5)
+
+  # b) "If the sum is not correct the method should raise an error"
+
+      # if statement comparing second argument from order() to total of values of items ordered from hash
+
+  # c) "otherwise the customer is sent a text saying that the order was placed successfully and that it will be delivered 1 hour from now"
+
+      # Twilio stuff
