7a2402e50231638136a6f5717e528db3452cbead
gabe <GabeMaker@users.noreply.github.com>
2015-01-03 15:49:26 +0000

    6 tests passing

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,11 @@
-DISHLIST = {pizza: 12, pasta: 10, salad: 7.50, garlic_bread: 5, cola: 2.5, mineral_water: 2}
+DISHLIST = {
+              pizza: 12,
+              pasta: 10,
+              salad: 7.50,
+              garlic_bread: 5,
+              cola: 2.5,
+              mineral_water: 2
+            }
 
 class User
 
@@ -13,5 +20,13 @@ class User
     @order << dish
     end 
   end
-  
+
+  def total
+    @order.inject(0) { |memo,item| memo + DISHLIST[item.to_sym]}
+  end
+
+  def place_order(total)
+    total == @order.inject(0) { |memo,item| memo + DISHLIST[item.to_sym]}
+  end
+
 end
