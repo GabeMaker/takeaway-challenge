ddef323681d540f124d1d4add393c1f216b54879
gabe <GabeMaker@users.noreply.github.com>
2015-01-04 21:45:02 +0000

    in progress: attempt to integrate Twilio functionality. uploaded but not working yet

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 DISHLIST = {
               pizza: 12,
               pasta: 10,
@@ -27,7 +29,22 @@ class User
 
   def place_order(total)
     if total == @order.inject(0) { |memo,item| memo + DISHLIST[item.to_sym]}
-      true
+      
+
+
+      @account_sid = #private
+      @auth_token = #private
+      @from_number = #private
+      @to_number = #private
+      # set up a client to talk to the Twilio REST API
+      @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+
+      time = Time.new
+      time = Time.new + 3600
+
+      @account = @client.account
+      @message = @account.messages.create({:from => @from_number, :to => @to_number, :body => 'Thank you! Your order was placed and will be delivered before 18:52'})
+      puts @message
     else raise 'Error'
     end
   end
